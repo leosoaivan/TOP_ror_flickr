03ed63412088b35dd23e8e29fa7246e9d87a19cf
L. Soai-Van <leo.soaivan@gmail.com>
2017-08-06 12:30:08 -0400

    Add styling and flash

--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -4,10 +4,8 @@ module FlickrHelper
     flickr.photos.search(user_id: user_id)
   end
 
-  private
-
-    def api_connection
-      FlickRaw.api_key = Figaro.env.flickr_api_key
-      FlickRaw.shared_secret = Figaro.env.flickr_secret
-    end
+  def api_connection
+    FlickRaw.api_key = Figaro.env.flickr_api_key
+    FlickRaw.shared_secret = Figaro.env.flickr_secret
+  end
 end
