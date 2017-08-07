190e1c82b63e06c3a9e0fcf8489c6c6b13ba322b
L. Soai-Van <leo.soaivan@gmail.com>
2017-08-07 19:42:38 -0400

    Resolve API issue
    * Set ENV variables for Heroku

--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -1,8 +1,6 @@
 module FlickrHelper
   def valid_user?(user_id)
-    FlickRaw.api_key = Figaro.env.flickr_api_key
-    FlickRaw.shared_secret = Figaro.env.flickr_secret
-    
+    api_connection
     begin
       true if flickr.profile.getProfile(user_id: user_id).is_a?(FlickRaw::Response)
     rescue
@@ -11,23 +9,17 @@ module FlickrHelper
   end
 
   def user_photos(user_id)
-    FlickRaw.api_key = Figaro.env.flickr_api_key
-    FlickRaw.shared_secret = Figaro.env.flickr_secret
-
+    api_connection
     flickr.photos.search(user_id: user_id)
   end
 
   def recent_photos
-    FlickRaw.api_key = Figaro.env.flickr_api_key
-    FlickRaw.shared_secret = Figaro.env.flickr_secret
-
+    api_connection
     flickr.photos.getRecent per_page: 50
   end
   
   def photo_info(photo)
-    FlickRaw.api_key = Figaro.env.flickr_api_key
-    FlickRaw.shared_secret = Figaro.env.flickr_secret
-    
+    api_connection    
     flickr.photos.getInfo(photo_id: photo.id)
   end
 
