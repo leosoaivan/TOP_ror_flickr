2a0b4eb5d7b1ece37f272aaa2804096b91cc05e6
L. Soai-Van <leo.soaivan@gmail.com>
2017-08-06 23:36:08 -0400

    Fix logic in view

--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -17,4 +17,12 @@ module FlickrHelper
     FlickRaw.api_key = Figaro.env.flickr_api_key
     FlickRaw.shared_secret = Figaro.env.flickr_secret
   end
+
+  def recent_photos
+    flickr.photos.getRecent per_page: 10
+  end
+
+  def photo_info(photo)
+    flickr.photos.getInfo(photo_id: photo.id)
+  end
 end
